d1aa8e354032dca43db936bb4e9a467804a34a6a
hoang <ngpbhoang1406@gmail.com>
2019-12-24 15:34:23 +0700

    Adding one more api

--- src/main/java/org/vgu/dm2schema/DM2Schema.java
+++ src/main/java/org/vgu/dm2schema/DM2Schema.java
@@ -57,9 +57,9 @@ import net.sf.jsqlparser.statement.create.table.CreateTable;
 public class DM2Schema {
 
     public static void main(String[] args) throws Exception {
-        File dataModelFile = new File("src/main/resources/genSQL/university_test.json");
-        File SQLschemaFile = new File("src/main/resources/genSQL/university_test.sql");
-        String databaseName = "unidb";
+        File dataModelFile = new File("src/main/resources/genSQL/car.json");
+        File SQLschemaFile = new File("src/main/resources/genSQL/car.sql");
+        String databaseName = "cardb";
 
         DataModel dataModel = new DataModel(new JSONParser()
             .parse(new FileReader(dataModelFile.getAbsolutePath())));
